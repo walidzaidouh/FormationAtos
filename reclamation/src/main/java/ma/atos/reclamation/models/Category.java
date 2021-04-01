1d1eaaa2d58154a0535d9b67c1d54921c79d410a
marouanemamdouh <mamdouhmarouane@gmail.com>
2021-04-01 10:23:39 +0100

    created entity category

---
+++ reclamation/src/main/java/ma/atos/reclamation/models/Category.java
@@ -0,0 +1,22 @@
+package ma.atos.reclamation.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    private int Id;
+    private String labelShort;
+    private String labelLong;
+
+
+
+}
