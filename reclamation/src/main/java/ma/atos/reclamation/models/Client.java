59d2286f983fb36ece48288fddb06155eb2cbad3
Abdelmoghit <elghali.abdelmoghit@gmail.com>
2021-04-01 09:53:33 +0200

    created Client entity

--- reclamation/src/main/java/ma/atos/reclamation/models/Client.java
+++ reclamation/src/main/java/ma/atos/reclamation/models/Client.java
@@ -1,5 +1,21 @@
 package ma.atos.reclamation.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name = "Client")
 public class Client {
+    @Id
+    private Long thirdPartyNumber;
+    private String cin;
+    private Long accountNumber;
+    private String ppm;
 
 }
