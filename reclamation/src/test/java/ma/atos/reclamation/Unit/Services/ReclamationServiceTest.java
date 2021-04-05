8b94ac8752492d895fab58a797ebcda66922737a
ach-raf <achraf.zouchou@gmail.com>
2021-04-05 10:48:48 +0000

    ReclamationServiceTest

--- reclamation/src/test/java/ma/atos/reclamation/Unit/Services/ReclamationServiceTest.java
+++ reclamation/src/test/java/ma/atos/reclamation/Unit/Services/ReclamationServiceTest.java
@@ -2,26 +2,23 @@ package ma.atos.reclamation.Unit.Services;
 
 import ma.atos.reclamation.Models.Reclamation;
 import ma.atos.reclamation.Repositories.ReclamationRepository;
-import ma.atos.reclamation.Services.ReclamationService;
 import ma.atos.reclamation.Services.ReclamationServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ReclamationServiceTest {
@@ -30,10 +27,12 @@ public class ReclamationServiceTest {
     @Mock
     private ReclamationRepository reclamationRepository;
     private Reclamation reclamation;
+
     @Before
     public void init() {
         reclamation = new Reclamation("XF555", "Urgent", Timestamp.from(Instant.now()), "Gestionnaire", "Body");
     }
+
     @Test
     public void testFindAllReclamationOk() {
         //Given
@@ -51,6 +50,23 @@ public class ReclamationServiceTest {
 
         assertEquals("XF555", returnedList.get(0).getReference());
         verify(reclamationRepository, times(1)).findAll();
+    }
+    @Test
+    public void testFindReclamationById() {
+        //Given
+        Optional<Reclamation> reclamationOptional = Optional.of(reclamation);
+
+        //When
+        when(reclamationRepository.findById("XF555")).thenReturn(reclamationOptional);
+
+        //Then
+        Optional<Reclamation> returned = reclamationService.findById("XF555");
+
+        assertNotNull(returned);
+        //assertEquals(reclamation, returned.get());
+        assertEquals("Urgent", returned.get().getPriority());
+        verify(reclamationRepository, times(1)).findById("XF555");
+
     }
 
 
