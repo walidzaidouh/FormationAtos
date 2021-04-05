1d3555d67404439f342060c04bead6ed696f214a
marouanemamdouh <mamdouhmarouane@gmail.com>
2021-04-05 09:58:54 +0000

    created category controller and implementation

--- reclamation/src/main/java/ma/atos/reclamation/Services/CategoryService.java
+++ reclamation/src/main/java/ma/atos/reclamation/Services/CategoryService.java
@@ -12,6 +12,9 @@ public interface CategoryService {
     public abstract Category add(Category category);
     public abstract Category update(int id , Category c);
     public abstract void deleteById(int id);
+
+    Optional<Category> findById(Integer Id);
+
     public abstract Optional<Category> findById(int Id);
 
     public abstract List<Category> findAll() ;
