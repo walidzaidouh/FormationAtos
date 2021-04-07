2472de7950996b18c8a303b7937718093b2a8f95
zineb louzi <zineblouzii@gmail.com>
2021-04-07 18:16:53 +0200

    FEAT-[DEV]- add CategoryDTO and ClientDTO classes

--- reclamation/src/main/java/ma/atos/reclamation/DTO/CategoryDTO.java
+++ reclamation/src/main/java/ma/atos/reclamation/DTO/CategoryDTO.java
@@ -6,8 +6,6 @@ import javax.persistence.Id;
 
 @Data
 public class CategoryDTO {
-    @javax.persistence.Id
-    private Long Id;
     private String labelShort;
     private String labelLong;
 
--- reclamation/src/main/java/ma/atos/reclamation/DTO/ClientDTO.java
+++ reclamation/src/main/java/ma/atos/reclamation/DTO/ClientDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class ClientDTO {
+
     private String cin;
     private Long accountNumber;
     private String ppm;
