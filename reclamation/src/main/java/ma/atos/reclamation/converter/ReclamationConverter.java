d7984c8e0839b3ff3398c8b526d423f8f57a948a
ach-raf <achraf.zouchou@gmail.com>
2021-04-08 10:44:39 +0000

    reclamation converter added list converter

--- reclamation/src/main/java/ma/atos/reclamation/converter/ReclamationConverter.java
+++ reclamation/src/main/java/ma/atos/reclamation/converter/ReclamationConverter.java
@@ -3,15 +3,28 @@ package ma.atos.reclamation.converter;
 import ma.atos.reclamation.dto.ReclamationDTO;
 import ma.atos.reclamation.models.Reclamation;
 import org.modelmapper.ModelMapper;
+import java.util.stream.Collectors;
+
+import java.util.List;
 
 public class ReclamationConverter {
+
     public ReclamationDTO reclamationToReclamationDto(Reclamation reclamation) {
         ModelMapper mapper = new ModelMapper();
+
         return mapper.map(reclamation, ReclamationDTO.class);
+    }
+
+    public List<ReclamationDTO> ReclamationToReclamationDto(List<Reclamation> reclamation) {
+        return	reclamation.stream().map(this::reclamationToReclamationDto).collect(Collectors.toList());
     }
 
     public Reclamation reclamationDtoToReclamation(ReclamationDTO reclamationDTO) {
         ModelMapper mapper = new ModelMapper();
         return mapper.map(reclamationDTO, Reclamation.class);
     }
+    public List<Reclamation> ReclamationDtoToReclamation(List<ReclamationDTO> reclamationDTO) {
+        return	reclamationDTO.stream().map(this::reclamationDtoToReclamation).collect(Collectors.toList());
+    }
+
 }
