42f309856e7280c1eaed23774500ca9865d73f11
Abdelmoghit <elghali.abdelmoghit@gmail.com>
2021-04-08 10:20:06 +0200

    Adding RatingConverter

--- reclamation/src/main/java/ma/atos/reclamation/converter/ClientConverter.java
+++ reclamation/src/main/java/ma/atos/reclamation/converter/ClientConverter.java
@@ -2,20 +2,18 @@ package ma.atos.reclamation.converter;
 
 import ma.atos.reclamation.dto.ClientDTO;
 import ma.atos.reclamation.models.Client;
-import org.modelmapper.ModelMapper;
 
 public class ClientConverter {
 
     public ClientDTO entityToDTO(Client client){
-        /* ClientDTO clientDto = new ClientDTO();
+         ClientDTO clientDto = new ClientDTO();
         clientDto.setCin(client.getCin());
         clientDto.setPpm(client.getPpm());
-        clientDto.setAccountNumber(client.getAccountNumber());*/
-
-        ModelMapper mapper =new ModelMapper();
-        return mapper.map(client, ClientDTO.class);
+        clientDto.setAccountNumber(client.getAccountNumber());
+        return clientDto;
 
     }
+
 
 
 }
---
+++ reclamation/src/main/java/ma/atos/reclamation/converter/RatingConverter.java
@@ -0,0 +1,18 @@
+package ma.atos.reclamation.converter;
+
+
+import ma.atos.reclamation.dto.RatingDTO;
+import ma.atos.reclamation.models.Rating;
+
+public class RatingConverter {
+    public RatingDTO entityToDto(Rating rating){
+        RatingDTO ratingDTO = new RatingDTO();
+        ratingDTO.setCommentaire(rating.getCommentaire());
+        ratingDTO.setNotes(ratingDTO.getNotes());
+        return ratingDTO;
+    }
+
+
+
+    }
+
