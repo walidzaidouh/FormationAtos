0ba5c93356f922a7fcc1604bd4010f45eacaf8ae
Abdelmoghit <elghali.abdelmoghit@gmail.com>
2021-04-08 10:25:28 +0200

    Adding RatingConverter

--- reclamation/src/main/java/ma/atos/reclamation/converter/RatingConverter.java
+++ reclamation/src/main/java/ma/atos/reclamation/converter/RatingConverter.java
@@ -5,7 +5,7 @@ import ma.atos.reclamation.dto.RatingDTO;
 import ma.atos.reclamation.models.Rating;
 
 public class RatingConverter {
-    public RatingDTO ratingToDto(Rating rating){
+    public RatingDTO ratingToDtoRating(Rating rating){
         RatingDTO ratingDTO = new RatingDTO();
         ratingDTO.setCommentaire(rating.getCommentaire());
         ratingDTO.setNotes(ratingDTO.getNotes());
