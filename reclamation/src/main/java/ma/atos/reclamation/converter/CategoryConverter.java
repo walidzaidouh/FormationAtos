0eff1d5bae8eccb59a775cef1a5f1a71d0ab2868
marouanemamdouh <mamdouhmarouane@gmail.com>
2021-04-08 14:16:37 +0000

    Category modified

--- reclamation/src/main/java/ma/atos/reclamation/converter/CategoryConverter.java
+++ reclamation/src/main/java/ma/atos/reclamation/converter/CategoryConverter.java
@@ -1,33 +1,35 @@
 package ma.atos.reclamation.converter;
 
 import ma.atos.reclamation.dto.CategoryDTO;
-import ma.atos.reclamation.dto.ReclamationDTO;
 import ma.atos.reclamation.models.Category;
-import ma.atos.reclamation.models.Reclamation;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class CategoryConverter {
-    public CategoryDTO categoryToCategoryDto(Category category) {
-        /* CategoryDTO categoryDto = new CategoryDTO();
-        categoryDto.setlabelShort(category.getlabelShort());
-        clientDto.setlabelLong(category.getlabelLong());*/
+@Component
+public class CategoryConverter{
 
-        ModelMapper mapper = new ModelMapper();
+    public CategoryDTO FromCategoryToCategoryDto(Category category){
+        ModelMapper mapper =new ModelMapper();
         return mapper.map(category, CategoryDTO.class);
     }
-    public List<CategoryDTO> CategoryToCategoryDto(List<Category> category) {
-        return	category.stream().map(this::categoryToCategoryDto).collect(Collectors.toList());
-    }
 
-    public Category categoryToCategoryDto(CategoryDTO categoryDTO) {
-        ModelMapper mapper = new ModelMapper();
+    public Category FromCategoryDtoToCategory(CategoryDTO categoryDTO){
+        ModelMapper mapper =new ModelMapper();
         return mapper.map(categoryDTO, Category.class);
     }
-    public List<Category> categoryToCategoryDto(List<CategoryDTO> categoryDTO) {
-        return	categoryDTO.stream().map(this::categoryToCategoryDto).collect(Collectors.toList());
+
+    public List<CategoryDTO> FromListCategoriesToListCategoriesDto(List<Category> categories){
+        ModelMapper mapper =new ModelMapper();
+        return categories.stream().map(this::FromCategoryToCategoryDto).collect(Collectors.toList());
+    }
+
+    public List<Category> FromListCategoriesToListCategoryDto(List<CategoryDTO> categoryDto){
+        ModelMapper mapper =new ModelMapper();
+        return categoryDto.stream().map(this::FromCategoryDtoToCategory).collect(Collectors.toList());
+
     }
 
 }
