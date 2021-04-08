a685d57365f37f989f8feba7f8f792eccb43b43c
zineb louzi <zineblouzii@gmail.com>
2021-04-08 10:59:49 +0200

    FEAT-[DEV]- update client converter class.

--- reclamation/src/main/java/ma/atos/reclamation/converter/ClientConverter.java
+++ reclamation/src/main/java/ma/atos/reclamation/converter/ClientConverter.java
@@ -3,33 +3,30 @@ package ma.atos.reclamation.converter;
 import ma.atos.reclamation.dto.ClientDTO;
 import ma.atos.reclamation.models.Client;
 import org.modelmapper.ModelMapper;
-
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class ClientConverter {
+public class ClientConverter{
 
     public ClientDTO ClientToClientDto(Client client){
-
         ModelMapper mapper =new ModelMapper();
         return mapper.map(client, ClientDTO.class);
-
-
     }
 
     public Client ClientDtoToClient(ClientDTO clientDTO){
-        Client client
-
         ModelMapper mapper =new ModelMapper();
         return mapper.map(clientDTO, Client.class);
     }
 
-    public List<ClientDTO> ListClientToListClientDto(List<Client> clients){
+    public List<ClientDTO> ListClientsToListClientsDto(List<Client> clients){
         ModelMapper mapper =new ModelMapper();
-        Stream.of(clients).map(c, Client.class).collect(Collectors.toList());
-     //   return mapper.map(ClientDTO, Client.class);
-        return null;
+        return clients.stream().map(this::ClientToClientDto).collect(Collectors.toList());
+    }
+
+    public List<Client> ListClientsDtoToListClients(List<ClientDTO> clientsDto){
+        ModelMapper mapper =new ModelMapper();
+        return clientsDto.stream().map(this::ClientDtoToClient).collect(Collectors.toList());
+
     }
 
 }
