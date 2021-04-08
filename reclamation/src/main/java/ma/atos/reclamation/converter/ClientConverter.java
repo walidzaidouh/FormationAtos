79e5c117bfe31ec7adee42a8c007d2498bb460f5
zineb louzi <zineblouzii@gmail.com>
2021-04-08 10:48:19 +0200

    FEAT-[DEV]- add client converter class

--- reclamation/src/main/java/ma/atos/reclamation/converter/ClientConverter.java
+++ reclamation/src/main/java/ma/atos/reclamation/converter/ClientConverter.java
@@ -4,18 +4,32 @@ import ma.atos.reclamation.dto.ClientDTO;
 import ma.atos.reclamation.models.Client;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class ClientConverter {
 
-    public ClientDTO entityToDTO(Client client){
-        /* ClientDTO clientDto = new ClientDTO();
-        clientDto.setCin(client.getCin());
-        clientDto.setPpm(client.getPpm());
-        clientDto.setAccountNumber(client.getAccountNumber());*/
+    public ClientDTO ClientToClientDto(Client client){
 
         ModelMapper mapper =new ModelMapper();
         return mapper.map(client, ClientDTO.class);
 
+
     }
 
+    public Client ClientDtoToClient(ClientDTO clientDTO){
+        Client client
+
+        ModelMapper mapper =new ModelMapper();
+        return mapper.map(clientDTO, Client.class);
+    }
+
+    public List<ClientDTO> ListClientToListClientDto(List<Client> clients){
+        ModelMapper mapper =new ModelMapper();
+        Stream.of(clients).map(c, Client.class).collect(Collectors.toList());
+     //   return mapper.map(ClientDTO, Client.class);
+        return null;
+    }
 
 }
