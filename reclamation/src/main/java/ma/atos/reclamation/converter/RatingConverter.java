1e47b872147d39b79197100bc4c80134dae3cdce
Abdelmoghit <elghali.abdelmoghit@gmail.com>
2021-04-08 10:52:45 +0200

    RatingConverter

--- reclamation/src/main/java/ma/atos/reclamation/converter/RatingConverter.java
+++ reclamation/src/main/java/ma/atos/reclamation/converter/RatingConverter.java
@@ -3,13 +3,24 @@ package ma.atos.reclamation.converter;
 
 import ma.atos.reclamation.dto.RatingDTO;
 import ma.atos.reclamation.models.Rating;
+import org.modelmapper.ModelMapper;
 
 public class RatingConverter {
-    public RatingDTO ratingToDtoRating(Rating rating){
-        RatingDTO ratingDTO = new RatingDTO();
-        ratingDTO.setCommentaire(rating.getCommentaire());
-        ratingDTO.setNotes(ratingDTO.getNotes());
-        return ratingDTO;
+
+        public RatingDTO ratingToRatingDto(Rating rating) {
+            ModelMapper mapper = new ModelMapper();
+            return mapper.map(rating, RatingDTO.class);
+        }
+
+        public Rating ratingDtoToRating(RatingDTO ratingDto) {
+            ModelMapper mapper = new ModelMapper();
+            return mapper.map(ratingDto , Rating.class);
+        }
+
     }
-}
+
+
+
+
+
 
