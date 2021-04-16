0e8b010efaf38a8a8c11ade1c96963b2c7ca8ae6
zineb louzi <zineblouzii@gmail.com>
2021-04-16 10:20:02 +0100

    FEAT-[FIX]- Add feign client annotation in the main method.

--- reclamation/src/main/java/ma/atos/reclamation/ReclamationApplication.java
+++ reclamation/src/main/java/ma/atos/reclamation/ReclamationApplication.java
@@ -1,9 +1,17 @@
 package ma.atos.reclamation;
 
-import org.springframework.boot.SpringApplication;
+import
+		org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ReclamationApplication {
 
 	public static void main(String[] args) {
